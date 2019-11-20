cdf921c8096c7af98fa5564ffbea7efee63d89e3
Evansii <vincent.casamayou@laposte.net>
2019-11-20 11:56:37 +0100

    init project

---
+++ GD_2/Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LevelGeneration : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] _tiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand=Random.Range(0, _tiles.Length);
+        Instantiate(_tiles[rand], transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
---
+++ GD_2/Assets/Scripts/Transition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour
+{
+    private Animator _anim;
+    // Start is called before the first frame update
+    void Start()
+    {  
+      _anim = GameObject.Find("clouds").GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+        Debug.Log("test");
+        StartCoroutine(LoadSceneTransition());
+        }
+    }
+
+    IEnumerator LoadSceneTransition()
+    {
+        _anim.SetTrigger("go");
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("Transi_test");
+    }
+}
+
