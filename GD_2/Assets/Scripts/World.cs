9fa6e7b75747bb1876980c54a96c83c4c46c7dd9
Evansii <vincent.casamayou@laposte.net>
2019-11-27 11:46:30 +0100

    Added prefab + world script

---
+++ GD_2/Assets/Scripts/World.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{   
+
+    [SerializeField]
+    private int _currentTurn = 1;
+
+    [SerializeField]
+    private int _playerTurn = 1;
+
+    [SerializeField]
+    private GameObject[] _playerList;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
