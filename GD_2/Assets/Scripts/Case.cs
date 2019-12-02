a4c815132c7d507f57ba59df1edc1b0efb63705d
Evansii <vincent.casamayou@laposte.net>
2019-12-02 12:20:13 +0100

    Add script Case

--- GD_2/Assets/Scripts/Case.cs
+++ GD_2/Assets/Scripts/Case.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Case : MonoBehaviour
 {
+    
+    public bool isCorrupted = false;
+
+    public string caseControl = "neutral";
+
+    public bool isActive = false;
+
+    private Transition _transiScript;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _transiScript = this.gameObject.GetComponent<Transition>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("oui");
+            activateCase();
+        }
+
+        if (isActive == true)
+        {
+            _transiScript.enabled = true;
+        }
     }
+
+    void activateCase()
+    {
+        isActive = true;
+
+    }
+
+    
+
+
 }
